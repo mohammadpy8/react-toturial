adc7e14499c76b8d8c934de811b4265231838eaa
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-14 11:24:36 +0330

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import './App.css';
-import Fliters from './components/Fliters';
-import MenuBar from './components/MenuBar';
-import Pagination from './components/Pagination';
-import ShowContacts from './components/ShowContacts';
-import Header from './components/Header';
-import Slider from './components/slider/Slider';
+
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Fliters />
-      <MenuBar />
-      <ShowContacts />
-      <Pagination />
-      <Slider />
-    </>
-  );
+    <div>
+      app
+    </div>
+  )
 }
 
-export default App;
+export default App
