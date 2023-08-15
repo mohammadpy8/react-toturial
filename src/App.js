7072412535be5c6249bee40018020854056b9843
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-15 13:27:23 +0330

    signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Router } from "react-router-dom";
 
 import ProductsContextProvider from "./context/ProductsContextProvider";
 import CartContextProvider from "./context/CartContextProvider";
@@ -11,19 +11,25 @@ import ProductDetails from "./components/ProductDetails";
 import Navbar from "./components/common/Navbar";
 import ShopCart from "./components/ShopCart";
 
+import SignUp from "./components/SignUp";
+
 function App() {
   return (
-    <ProductsContextProvider>
-      <CartContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/products" element={<Store />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<ShopCart />} />
-          <Route path="*" element={<Navigate to="/products" />} />
-        </Routes>
-      </CartContextProvider>
-    </ProductsContextProvider>
+    // <ProductsContextProvider>
+    //   <CartContextProvider>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/products" element={<Store />} />
+    //       <Route path="/products/:id" element={<ProductDetails />} />
+    //       <Route path="/cart" element={<ShopCart />} />
+    //       <Route path="*" element={<Navigate to="/products" />} />
+    //     </Routes>
+    //   </CartContextProvider>
+    // </ProductsContextProvider>
+          <Routes>
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="*" element={<Navigate to="/signup" replace/>} />
+          </Routes>
   );
 }
 
---
+++ src/components/SignUp.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+
+    const changeHandler = (event) => {
+      
+        if (event.target.name === "isAccepted") {
+
+            setData({ ...data, [event.target.name]: event.target.checked });
+            
+        } else {
+
+            setData({ ...data, [event.target.name]: event.target.value });
+        }
+
+  };
+
+  return (
+    <div>
+      <form>
+        <h2>Sign Up</h2>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>email</label>
+          <input
+            type="text"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>password</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>confirmPassword</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>I accepted privcy policy</label>
+          <input
+            type="checkbox"
+            name="isAccepted"
+            value={data.isAccepted}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <Link to="/login">Login</Link>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
