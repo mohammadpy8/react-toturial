d05023c581aff4ee3ae0ea9f70ce784d2d1df717
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-16 07:57:17 +0330

    fetch-users

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
 import counterSlice from "../features/counter/counterSlice";
+import usersSlice from "../features/users/usersSlice";
 
 const store = configureStore({
     reducer: {
         counter: counterSlice,
+        users: usersSlice,
     },
     middleware :(getDefaultMiddleware) => getDefaultMiddleware().concat(logger) 
 });
